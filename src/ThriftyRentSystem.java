1a4a0410999c1ff4cdb4578f882bf1393c7ecd9a
RMIT <RMIT@Roshans-MacBook-Air.local>
2019-04-02 20:55:29 +1100

    created the menu

--- src/ThriftyRentSystem.java
+++ src/ThriftyRentSystem.java
@@ -1,10 +1,49 @@
+import java.util.Scanner;
+
 import util.DateTime;
 
 public class ThriftyRentSystem {
 
 	public void runProgram() {
-		double value = 200.3456;
-		System.out.printf("Value: %.2f", value);
+		
+		Vehicle[] vehicles = new Vehicle[50];
+		
+		
+		
+		while(true){
+			try{
+			systemMenu();
+			Scanner input = new Scanner(System.in);
+			int num = input.nextInt();
+			switch(num){
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+				case 7:
+					System.exit(0);
+					break;
+				default:
+					System.err.println("Invalid Input: Please enter an input between 1 and 7");
+					System.out.println("");
+					break;
+			}
+			}catch(Exception e){
+				System.err.println("Invalid Input: Please enter a number from the menu");
+				System.out.println("");
+			}
+		}
+		/*
+//		double value = 200.3456;
+//		System.out.printf("Value: %.2f", value);
 //		DateTime test = new DateTime();
 //		System.out.println(test);
 		
@@ -32,5 +71,20 @@ public class ThriftyRentSystem {
 
 				//You're encouraged to explore other constructors and methods
 				//of the DateTime class
+				 * 
+				 */
+	}
+	
+	private static void systemMenu(){
+		System.out.println("****  ThriftyRent SYSTEM MENU  ****");
+		System.out.println("");
+		System.out.println("Add vehicle:                      1");
+		System.out.println("Rent vehicle:                     2");
+		System.out.println("Return vehicle:                   3");
+		System.out.println("Vehicle Maintainance:             4");
+		System.out.println("Complete Maintainance:            5");
+		System.out.println("Display All Vehicle:              6");
+		System.out.println("Exit Program:                     7");
+		System.out.println("Enter your choice:                 ");
 	}
 }
